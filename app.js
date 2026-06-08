f29808d6bc57db2b25449791628ffed1f73811b0
Pavan Kumar <pavankumar82371@gmail.com>
2026-06-09 00:21:46 +0530

    Step 9: Adding Row Btn

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const row = 20;
+let row = 20;
 const col = 10;
 
 const gridContainer = document.getElementById('grid-container');
@@ -18,6 +18,44 @@ const savedData = JSON.parse(localStorage.getItem("spreadsheetData")) || {};
 // if(!savedData){
 //     savedData = {};
 // }
+
+function setupCell(cell, cellId){
+
+    cell.dataset.cellId = cellId;
+    cell.contentEditable = true;
+
+        // Step 3 and 4: Detect and highlight which cell is selected
+        cell.addEventListener('click',(e)=>{
+            // console.log(e.target.dataset.cellId);
+
+            cellInfo.textContent = `Selected Cell: ${e.target.dataset.cellId}`
+            // selected colour changing style
+
+            if(selectedcell){
+                selectedcell.classList.remove("selected-cell");
+            }
+
+            e.target.classList.add("selected-cell");
+
+            selectedcell = e.target;
+
+            formulaInput.value = e.target.textContent;
+
+            // console.log(selectedcell);
+        })
+
+        // Step 5: Saves data when a cell is edited
+        cell.addEventListener('input',(e)=>{
+            const cellId = e.target.dataset.cellId;
+            const val = e.target.textContent.trim();
+
+            if(val) sheetData[cellId] = val;
+            else delete sheetData[cellId];
+
+            // console.log(sheetData);
+            })
+        }
+        
 
 
 for( let i=0; i<=row; i++){
@@ -44,7 +82,7 @@ for( let i=0; i<=row; i++){
             cell.classList.add("cell");
             const columnLetter = String.fromCharCode(64+j);
             const cellId = `${columnLetter}${i}`
-            cell.dataset.cellId = cellId;
+            
             
             // Restore values while creating cells.
             // cell mein b jyga and sheetData mein b.
@@ -53,44 +91,13 @@ for( let i=0; i<=row; i++){
                 sheetData[cellId] = savedData[cellId];
             }
 
-            cell.contentEditable = true;
-
-            // Step 3 and 4: Detect and highlight which cell is selected
-            cell.addEventListener('click',(e)=>{
-                // console.log(e.target.dataset.cellId);
-
-                cellInfo.textContent = `Selected Cell: ${e.target.dataset.cellId}`
-                // selected colour changing style
-
-                if(selectedcell){
-                    selectedcell.classList.remove("selected-cell");
-                }
-
-                e.target.classList.add("selected-cell");
-
-                selectedcell = e.target;
-
-                formulaInput.value = e.target.textContent;
-
-                // console.log(selectedcell);
-            })
-
-            // Step 5: Saves data when a cell is edited
-            cell.addEventListener('input',(e)=>{
-                const cellId = e.target.dataset.cellId;
-                const val = e.target.textContent.trim();
-
-                if(val) sheetData[cellId] = val;
-                else delete sheetData[cellId];
-
-                // console.log(sheetData);
-            })
+            setupCell(cell,cellId);
         }
-        
+            
         gridContainer.appendChild(cell);
 
+        }
     }
-}
 
 formulaInput.addEventListener("input",()=>{
 
@@ -108,6 +115,39 @@ formulaInput.addEventListener("input",()=>{
     else delete sheetData[cellId];
 
 })
+
+// Buttons
+
+const addRowBtn = document.getElementById("add-row-btn");
+
+addRowBtn.addEventListener("click",()=>{
+
+    row++;
+
+    const rowHeader = document.createElement("div");
+    // given class = row-header , so every css and js logic will done on this given class "row-header"
+
+    rowHeader.classList.add("row-header");
+    rowHeader.textContent = row;
+
+    gridContainer.appendChild(rowHeader);
+
+    for(let j=1; j<=col; j++){
+
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        const columnLetter = String.fromCharCode(64+j);
+        const cellId = `${columnLetter}${row}`;
+
+        setupCell(cell, cellId);
+
+        gridContainer.appendChild(cell);
+    }
+
+    // console.log(row);
+})
+
 
 const saveBtn = document.getElementById('save-btn');
 
